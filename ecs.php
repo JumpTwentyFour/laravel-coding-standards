01a6ec26218ab500832087ff532c8c8bbc0ff8c0
Alex Williams <alex@jump24.co.uk>
2021-05-24 08:39:06 +0100

    UPDATE: Fix tests

--- ecs.php
+++ ecs.php
@@ -1,18 +1,9 @@
 <?php
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::PATHS, [
-        __DIR__ . '/app',
-        __DIR__ . '/config',
-        __DIR__ . '/database',
-        __DIR__ . '/routes',
-        __DIR__ . '/tests',
-    ]);
+    $containerConfigurator->parameters();
 
     $containerConfigurator->import(__DIR__ . '/vendor/jumptwentyfour/php-coding-standards/ecs.php');
 };
